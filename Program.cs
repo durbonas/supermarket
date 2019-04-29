584039fec01aa9947295cdd44bb972afa2196145
David Urbonas <durbonas@bioscript.ca>
2019-04-29 14:36:50 -0300

    print out of added shopping items added

--- Program.cs
+++ Program.cs
@@ -14,13 +14,17 @@ namespace Supermarket
             //Cycle through shopping list and add items
             foreach (var item in args)
             {   
-                //For every shopping list item create new GroceryItem Object
+                //For every shopping item create new GroceryItem Object
                 var GroceryPurchase = new GroceryItem(item);
-                Console.WriteLine($"Buying {item}");
-                Console.WriteLine("{0},{1}", GroceryPurchase.name, GroceryPurchase.price);
+
                 //Add GroceryItem to my shopping list
                 myGroceryList.AddGroceryItem(GroceryPurchase);
-                Console.WriteLine("My Grocery List {0}", myGroceryList.GroceryItemsList[0].name);
+            }
+
+            //Print out my shopping list
+            foreach (var item in myGroceryList.GroceryItemsList)
+            {
+                System.Console.WriteLine($"{item.name}...............{item.price}");
             }
 
         }
